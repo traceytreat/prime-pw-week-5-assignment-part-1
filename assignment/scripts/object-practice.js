d223d8d77e8b049ceee1bb0cc9f4c47f26476481
Tracey Treat <tracey_treat@yahoo.com>
2022-10-23 06:59:50 -0500

    favThreeFoods array console.log

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -32,7 +32,8 @@ console.log(fullName);
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-
+console.log(me.favThreeFoods[0]);
+console.log(me.favThreeFoods[2]);
 
 
 /* 4. Change a property of an existing object.
